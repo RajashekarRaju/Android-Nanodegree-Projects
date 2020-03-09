fe1a78c1fe3b6b0dac63aae0de3dd823875a3989
rajas <rajashekarraju123@gmail.com>
2020-03-09 08:46:11 +0530

    Added ListFragment to bottom navigation view

--- app/src/main/java/com/developersbreach/xyzreader/view/MainActivity.java
+++ app/src/main/java/com/developersbreach/xyzreader/view/MainActivity.java
@@ -13,14 +13,12 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.developersbreach.xyzreader.R;
 import com.developersbreach.xyzreader.databinding.ActivityMainBinding;
-import com.developersbreach.xyzreader.utils.ThemePreferencesManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private NavController mNavigationController;
-    private ThemePreferencesManager mPreferenceManager;
     private ActivityMainBinding mBinding;
 
     @Override
@@ -29,9 +27,6 @@ public class MainActivity extends AppCompatActivity {
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mNavigationController = Navigation.findNavController(this, R.id.nav_host_fragment);
         mBinding.bottomNavigation.setOnNavigationItemSelectedListener(new NavigationListener());
-        mPreferenceManager = new ThemePreferencesManager(this);
-        mPreferenceManager.applyTheme();
-
         mNavigationController.addOnDestinationChangedListener((controller, destination, arguments) -> {
             if (destination.getId() == R.id.articleDetailFragment) {
                 mBinding.bottomNavigation.setVisibility(View.INVISIBLE);
@@ -39,11 +34,6 @@ public class MainActivity extends AppCompatActivity {
                 mBinding.bottomNavigation.setVisibility(View.VISIBLE);
             }
         });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        return mNavigationController.navigateUp();
     }
 
     private class NavigationListener implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -59,17 +49,20 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.articleFavoritesFragment:
 
+                case R.id.articleListFragment:
                 case R.id.searchArticleFragment:
+                case R.id.articleFavoritesFragment:
+                case R.id.accountFragment:
                     NavigationUI.onNavDestinationSelected(item, mNavigationController);
-                    break;
-
-                case R.id.theme_bottom_menu_item:
-                    mPreferenceManager.showThemePopUp(mBinding.bottomNavigation);
                     break;
             }
             return true;
         }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        return mNavigationController.navigateUp();
     }
 }
