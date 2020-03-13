c53813e85e28a805e2f865de12bc9a4613aa1691
rajas <rajashekarraju123@gmail.com>
2020-03-14 03:46:41 +0530

    Added Navigation Transition animations

--- app/src/main/java/com/developersbreach/xyzreader/view/list/ArticleListFragment.java
+++ app/src/main/java/com/developersbreach/xyzreader/view/list/ArticleListFragment.java
@@ -1,42 +1,35 @@
 package com.developersbreach.xyzreader.view.list;
 
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.developersbreach.xyzreader.R;
 import com.developersbreach.xyzreader.databinding.FragmentArticleListBinding;
-import com.developersbreach.xyzreader.utils.SpaceItemDecoration;
+import com.developersbreach.xyzreader.model.Article;
+import com.developersbreach.xyzreader.utils.ArticleAnimations;
+import com.developersbreach.xyzreader.utils.RecyclerViewItemDecoration;
 import com.developersbreach.xyzreader.utils.ThemePreferencesManager;
 import com.developersbreach.xyzreader.viewModel.ArticleListViewModel;
 
-import java.util.Objects;
+import java.util.List;
 
 
 public class ArticleListFragment extends Fragment {
 
-    private RecyclerView mArticleRecyclerView;
     private ArticleListViewModel mViewModel;
     private FragmentArticleListBinding mBinding;
     private ThemePreferencesManager mPreferenceManager;
 
-    private static final String COMPLETED_ANIMATION_PREF_NAME = "ANIMATION_LINEAR_OUT";
-
-    /**
-     * Using preferences with editor decide whether to change the default value in string
-     * COMPLETED_ANIMATION_PREF_NAME to show or hide the animation.
-     */
-    private SharedPreferences mSharedPreferences;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -44,59 +37,40 @@ public class ArticleListFragment extends Fragment {
         mBinding = DataBindingUtil.inflate(inflater,
                 R.layout.fragment_article_list, container, false);
 
-        mArticleRecyclerView = mBinding.articlesRecyclerView;
-        int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.recycler_view_spacing_dimen);
-        mArticleRecyclerView.addItemDecoration(new SpaceItemDecoration(spacingInPixels));
-        setFragmentToolbar(getContext());
+        setToolbar(getContext(), mBinding.toolbarContentArticlesHeader.headerThemeSwitcherImageView);
+        RecyclerViewItemDecoration.setItemSpacing(getResources(), mBinding.articlesRecyclerView);
         mBinding.setLifecycleOwner(this);
         return mBinding.getRoot();
     }
 
     @Override
-    public void onResume() {
-        super.onResume();
-        animateForFirstTimeOnly();
-    }
-
-    // Until value in sharedPreference won't effect, we continue showing animation to user.
-    private void animateForFirstTimeOnly() {
-        // Getting our shared preference in primate mode.
-        mSharedPreferences = Objects.requireNonNull(getActivity()).getPreferences(Context.MODE_PRIVATE);
-        // Check for change in default value, if false returned then no effect happens.
-        if (!mSharedPreferences.getBoolean(COMPLETED_ANIMATION_PREF_NAME, false)) {
-            // Starts th animation with the properties we set.
-            startLinearAnimation();
-        }
-    }
-
-    private void startLinearAnimation() {
-        // Occurs transition from bottom to top at 800dp.
-        mBinding.fragmentListParent.setTranslationY(800);
-        // Animate with duration, height.
-        mBinding.fragmentListParent.animate().translationY(0f).setDuration(1000L).start();
-        // After finishing the animation,change boolean value inside String with editor.
-        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
-        mEditor.putBoolean(COMPLETED_ANIMATION_PREF_NAME, true);
-        mEditor.apply();
-    }
-
-    private void setFragmentToolbar(Context context) {
-        mBinding.toolbarContentArticlesHeader.headerThemeSwitcherImageView.setVisibility(View.VISIBLE);
-        mPreferenceManager = new ThemePreferencesManager(context);
-        mPreferenceManager.applyTheme();
-        mBinding.toolbarContentArticlesHeader.headerThemeSwitcherImageView.setOnClickListener(view ->
-                mPreferenceManager.showThemePopUp(mBinding.toolbarContentArticlesHeader.headerThemeSwitcherImageView));
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ArticleListViewModel.class);
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ArticleListViewModel.class);
+        mViewModel.articles().observe(getViewLifecycleOwner(), this::onArticlesChanged);
+    }
 
-        mViewModel.getArticleList().observe(getViewLifecycleOwner(), articles -> {
-            ArticleAdapter adapter = new ArticleAdapter(mViewModel, this);
-            adapter.submitList(articles);
-            mArticleRecyclerView.setAdapter(adapter);
-        });
+    private void onArticlesChanged(List<Article> articleList) {
+        ArticleAdapter adapter = new ArticleAdapter(mViewModel, this);
+        adapter.submitList(articleList);
+        mBinding.articlesRecyclerView.setAdapter(adapter);
+    }
+
+    private void setToolbar(Context context, ImageView themeSwitcherImageView) {
+        mPreferenceManager = new ThemePreferencesManager(context);
+        mPreferenceManager.applyDayNightTheme();
+        themeSwitcherImageView.setOnClickListener(view ->
+                mPreferenceManager.showThemeSwitcherPopUp(themeSwitcherImageView));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ArticleAnimations.startLinearAnimation(mBinding.articlesRecyclerView);
     }
 }
